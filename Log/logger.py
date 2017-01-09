5f659274912ca5d14698bf0c666fdb55ff6dfc72
XuZhaoning <1933266722@qq.com>
2017-01-09 15:11:22 +0800

    更改log写入编码方式
    更改log写入编码方式

--- Log/logger.py
+++ Log/logger.py
@@ -3,5 +3,10 @@ import datetime
 
 def write(level, content):
     with open('./Log/log/{}'.format(datetime.date.today()), 'a') as f:
-        f.write('[{}][{}]{}'.format(level, datetime.datetime.now(), content))
+        f.write('[{}][{}]{}'.format(level, datetime.datetime.now(), content.encode('utf8')))
+        f.write('\n')
+
+
+def space():
+    with open('./Log/log/{}'.format(datetime.date.today()), 'a') as f:
         f.write('\n')
