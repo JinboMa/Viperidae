76448c425f1dd47993470847e6995cbe6045da85
JinboMa <596756194@qq.com>
2017-01-11 00:17:30 +0800

    修改时间,导航,加入Blog页面

--- vue-develop/src/App.vue
+++ vue-develop/src/App.vue
@@ -27,12 +27,18 @@ export default {
 </script>
 
 <style lang="stylus">
+$AllWidth = 1050px
 *
 	margin 0
 	padding 0
-	font-family "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif
-div
+	outline none
 	box-sizing border-box
+	font-family "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif
+html
+body
+#app
+	height 100%
 a
 	text-decoration none
+	color inherit
 </style>
