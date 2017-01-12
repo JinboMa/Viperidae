47a47eb319e412fce2c31a60276450d73e9a2fd4
XuZhaoning <1933266722@qq.com>
2017-01-12 10:59:47 +0800

    解决跨域问题
    解决跨域问题

--- Handler/BaseHandler.py
+++ Handler/BaseHandler.py
@@ -4,4 +4,5 @@ import tornado.web
 class BaseHandler(tornado.web.RequestHandler):
     def write(self, chunk):
         self.set_header('Access-Control-Allow-Origin', 'http://23.105.208.8')
+        self.add_header('Access-Control-Allow-Credentials', 'true')
         super(BaseHandler, self).write(chunk)
