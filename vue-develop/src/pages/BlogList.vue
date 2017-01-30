de51072524da1293f48eddd8332e1a02d4bb6861
JinboMa <596756194@qq.com>
2017-01-30 17:52:13 +0800

    添加blog相关请求

--- vue-develop/src/pages/BlogList.vue
+++ vue-develop/src/pages/BlogList.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="blogList content" v-loading="loading" element-loading-text="正在提交,请稍等">
+<div class="blogList content" v-loading="loading" element-loading-text="正在请求,请稍等">
 	<el-row>
 		<el-col :span="12">
-			<blog-content v-for="blog in formData" :blogMsg="blog"></blog-content>
+			<blog-content v-for="(blog,item) in formData" :blogMsg="blog" :blogId="item"></blog-content>
 		</el-col>
 	</el-row>
 </div>
@@ -14,47 +14,50 @@ export default {
 	data(){
 		return {
 			loading : false,
-			formData : [
-				{
+			formData : {
+				5:{
 					title : "title,标题,test overflow,test overflow,test overflow,test overflow,test overflow,test overflow,test overflow,test overflow,test overflow",
 					description : "test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ; test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ;test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ; test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz",
-					rate : "3.7",
+					rate : 3.7,
 					author : "马金博",
 					createTime : "2017-01-30 14:57"
 				},
-				{
+				2:{
 					title : "title1,标题",
 					description : "test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ; test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ;test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ; test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz",
-					rate : "3.7",
+					rate : 3.7,
 					author : "马金博",
 					createTime : "2017-01-30 14:57"
 				},
-				{
+				3:{
 					title : "title2,标题",
 					description : "test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ; test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ;test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz ; test content,test length for 100,abcdefg higklmn opqrst uvwxyz,abcdefg hijklmn opqrst uvwxyz",
-					rate : "3.7",
+					rate : 3.7,
 					author : "马金博",
 					createTime : "2017-01-30 14:57"
 				}
-			]
+			}
 		}
 	},
 	components : {
 		BlogContent
 	},
+	created : function(){
+		this.getMessage()
+	},
 	methods : {
-		//发送登录请求
-		postMessage : function(){
+		//发送获取博客列表请求
+		getMessage : function(){
 			this.loading = true
 			this.ajax(this.setAjax("blogList",this.formData,this.success,this.fail))
 		},
-		//提交成功
+		//获取成功
 		success : function(res){
-			this.$router.push('/')
+			this.formData = res.message
 		},
-		//提交失败
+		//获取失败
 		fail : function(res){
-			console.log("失败")
+			console.log("失败",res)
 		}
 	}
 }
